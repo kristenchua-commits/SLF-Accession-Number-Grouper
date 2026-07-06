81fa971907c56befd688983f40699e47d541fc44
kristenchua-commits <kristen.chua@ucop.edu>
2026-07-06 09:59:16 -0700

    improved prefix and postfix detection and validation

--- SLF Accession Number _ Prefix Grouper.R
+++ SLF Accession Number _ Prefix Grouper.R
@@ -8,14 +8,34 @@ df <- read_csv(
   show_col_types = FALSE
 )
 
-# Create a new column containing the letter prefix
+# Create new columns
 prefix <- df %>%
   mutate(
-    Barcode_Prefix = str_extract(Barcode, "^[A-Za-z]+")
-  )
+    Barcode_Prefix = str_extract(Barcode, "^[A-Za-z]+"),
+    Barcode_Postfix = str_remove(Barcode, "^[A-Za-z]+"),
+    Barcode_Postfix_Num = as.numeric(str_extract(Barcode_Postfix, "^\\d+"))
+  ) %>%
+  group_by(Barcode_Prefix) %>%
+  arrange(Barcode_Postfix_Num, .by_group = TRUE) %>%
+  mutate(
+    Prev_Postfix = lag(Barcode_Postfix_Num),
+    Next_Postfix = lead(Barcode_Postfix_Num),
+    Barcode_Postfix_Diff = pmin(
+      abs(Barcode_Postfix_Num - Prev_Postfix),
+      abs(Next_Postfix - Barcode_Postfix_Num),
+      na.rm = TRUE
+    ),
+    Barcode_Postfix_Diff = if_else(
+      is.infinite(Barcode_Postfix_Diff),
+      NA_real_,
+      Barcode_Postfix_Diff
+    )
+  ) %>%
+  ungroup() %>%
+  select(-Barcode_Postfix_Num, -Prev_Postfix, -Next_Postfix)
 
 # View the results
 View(prefix)
 
 # Save the updated file
-write_csv(prefix, "barcode_prefixes.csv")
+write_csv(prefix, "barcode_prefixes&postfix.csv")
